9c2ba5858e285942890a4fbc6edce7d583d6f0df
Philip <filip2unine@yahoo.co.uk>
2019-11-27 20:58:11 +0100

    added line chart feature

--- com.npkompleet.dps.application/src/com/npkompleet/dps/application/parts/ChartPart.java
+++ com.npkompleet.dps.application/src/com/npkompleet/dps/application/parts/ChartPart.java
@@ -1,8 +1,11 @@
 package com.npkompleet.dps.application.parts;
 
+import java.util.stream.Stream;
+
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
+import org.eclipse.e4.core.di.annotations.Creatable;
 import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
@@ -15,12 +18,21 @@ import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.swtchart.Chart;
+import org.eclipse.swtchart.ILineSeries;
+import org.eclipse.swtchart.ISeries.SeriesType;
 
+import com.npkompleet.dps.application.util.ChartData;
+import com.npkompleet.dps.application.util.ValueFileReader;
 
+@Creatable
 public class ChartPart {
 	private Text text;
     private Chart chart;
     
+    @Inject
+    private ChartData chartData;
+    @Inject
+    private ValueFileReader fileReader;
 
     @PostConstruct
     public void createControls(Composite parent) {
@@ -38,18 +50,43 @@ public class ChartPart {
             public void widgetSelected(SelectionEvent e) {
             	// Open a file dialog that reads only JSON files
                 FileDialog dialog = new FileDialog(new Shell(SWT.CENTER), SWT.OPEN);
-                dialog.setFilterExtensions(new String[] {".json"});
+                dialog.setFilterExtensions(new String[] {"*.txt"});
                 dialog.open();
+                String filePath = dialog.getFilterPath().concat("\\").concat(dialog.getFileName());
+                text.setText(filePath);
                 
-                text.setText(dialog.getFilterPath().concat("\\").concat(dialog.getFileName()));
+                chartData = fileReader.parseFile(filePath);
+                
+                // set titles
+                chart.getTitle().setText(chartData.getTitle());
+                chart.getAxisSet().getXAxis(0).getTitle().setText(chartData.getxLabel());
+                chart.getAxisSet().getYAxis(0).getTitle().setText(chartData.getyLabel());
+
+                
+                // Check if it is a line chart and if so covert the x-axis values of
+                // the chart to double array format instead of a string array
+                if (chartData.getType().equals("line")) {
+                	// create line series
+                    ILineSeries lineSeries = (ILineSeries) chart.getSeriesSet()
+                        .createSeries(SeriesType.LINE, "line series");
+                    double[] xSeries;
+                    
+                	xSeries = Stream.of(chartData.getxValues())
+                			.map(x -> Double.parseDouble(x))
+        					.mapToDouble(Double :: doubleValue)
+        					.toArray();
+                	lineSeries.setXSeries(xSeries);
+                    lineSeries.setYSeries(chartData.getyValues());
+                }
+                chart.redraw();
+                chart.getAxisSet().adjustRange();
                 
             }
         });
-        
+
+
         chart = new Chart(parent, SWT.NONE);
         chart.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
-
-
     }
 
     @Focus
---
+++ com.npkompleet.dps.application/src/com/npkompleet/dps/application/util/ChartData.java
@@ -0,0 +1,66 @@
+package com.npkompleet.dps.application.util;
+
+import org.eclipse.e4.core.di.annotations.Creatable;
+
+@Creatable
+public class ChartData {
+	private String title;
+	private String type;
+	private String[] xValues;
+	private double[] yValues;
+	private String xLabel;
+	private String yLabel;
+
+	public ChartData() {
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String[] getxValues() {
+		return xValues;
+	}
+
+	public void setxValues(String[] xValues) {
+		this.xValues = xValues;
+	}
+
+	public double[] getyValues() {
+		return yValues;
+	}
+
+	public void setyValues(double[] yValues) {
+		this.yValues = yValues;
+	}
+
+	public String getxLabel() {
+		return xLabel;
+	}
+
+	public void setxLabel(String xLabel) {
+		this.xLabel = xLabel;
+	}
+
+	public String getyLabel() {
+		return yLabel;
+	}
+
+	public void setyLabel(String yLabel) {
+		this.yLabel = yLabel;
+	}
+	
+
+}
