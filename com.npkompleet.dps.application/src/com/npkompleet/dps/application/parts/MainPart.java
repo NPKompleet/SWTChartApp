297d9027aa7773b9c3a45e892cab119a8a008768
Philip <filip2unine@yahoo.co.uk>
2019-12-30 17:54:21 +0100

    Added SWTBot testing

--- com.npkompleet.dps.application/src/com/npkompleet/dps/application/parts/MainPart.java
+++ com.npkompleet.dps.application/src/com/npkompleet/dps/application/parts/MainPart.java
@@ -1,6 +1,5 @@
 package com.npkompleet.dps.application.parts;
 
-
 import javax.annotation.PostConstruct;
 
 import org.eclipse.e4.ui.di.Focus;
@@ -21,41 +20,40 @@ import com.npkompleet.dps.application.util.ChartDataSingleton;
 public class MainPart {
 	private Label label;
 	private Text text;
-	
+
 	@PostConstruct
 	public void create(Composite parent) {
-		
+
 		parent.setLayout(new GridLayout(2, false));
-		
+
 		label = new Label(parent, SWT.NONE);
 		label.setText("Please load the file to read from");
 		label.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 2, 1));
 
-        text = new Text(parent, SWT.BORDER);
-        text.setMessage("Choose file...");
-        text.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-        
-        
-        Button button = new Button(parent, SWT.PUSH);
-        button.setText("File");
-        
-        button.addSelectionListener(new SelectionAdapter() {
-            @Override
-            public void widgetSelected(SelectionEvent e) {
-            	// Open a file dialog that reads only .amxmi files
-                FileDialog dialog = new FileDialog(new Shell(SWT.CENTER), SWT.OPEN);
-                dialog.setFilterExtensions(new String[] {"*.amxmi"});
-                dialog.open();
-                String filePath = dialog.getFilterPath().concat("\\").concat(dialog.getFileName());
-                text.setText(filePath);
-                
-                ChartDataSingleton.getInstance().setFilePath(filePath);
-            	}        
-            });
+		text = new Text(parent, SWT.BORDER);
+		text.setMessage("Choose file...");
+		text.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 
+		Button button = new Button(parent, SWT.PUSH);
+		button.setText("File");
+
+		button.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				// Open a file dialog that reads only .amxmi files
+				FileDialog dialog = new FileDialog(new Shell(SWT.CENTER), SWT.OPEN);
+				dialog.setText("Open Model File");
+				dialog.setFilterExtensions(new String[] { "*.amxmi" });
+				dialog.open();
+				String filePath = dialog.getFilterPath().concat("\\").concat(dialog.getFileName());
+				text.setText(filePath);
+
+				ChartDataSingleton.getInstance().setFilePath(filePath);
+			}
+		});
 
 	}
-	
+
 	@Focus
 	void onFocus() {
 		label.setFocus();
---
+++ com.npkompleet.dps.application/src/test/PartUITest.java
@@ -0,0 +1,58 @@
+package test;
+
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swtbot.e4.finder.matchers.WidgetMatcherFactory;
+import org.eclipse.swtbot.swt.finder.SWTBot;
+import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.eclipse.swtchart.Chart;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(SWTBotJunit4ClassRunner.class)
+public class PartUITest {
+
+	private static SWTBot bot;
+
+	@BeforeClass
+	public static void beforeClass() throws Exception {
+		bot = new SWTBot();
+	}
+
+	@Test
+	public void testThatEverythingDisplays() {
+		bot.cTabItem("Main Page");
+		bot.cTabItem("Label Size");
+		bot.cTabItem("Activation Pattern");
+		Assert.assertTrue(bot.cTabItem("Main Page").isActive());
+	}
+
+	@Test
+	public void testMainPage() {
+		bot.cTabItem("Main Page").activate();
+		bot.label("Please load the file to read from");
+		Text text = bot.widget(WidgetMatcherFactory.widgetOfType(Text.class));
+		Assert.assertNotNull(text);
+		Assert.assertEquals("Choose file...", text.getMessage());
+		bot.button("File");
+	}
+
+	@Test
+	public void testLabelSize() {
+		bot.cTabItem("Label Size").activate();
+		Assert.assertEquals(1, bot.widgets(WidgetMatcherFactory.widgetOfType(Chart.class)).size());
+	}
+
+	@Test
+	public void testActivationPattern() {
+		bot.cTabItem("Activation Pattern").activate();
+		Assert.assertEquals(1, bot.widgets(WidgetMatcherFactory.widgetOfType(Chart.class)).size());
+	}
+
+	@AfterClass
+	public static void sleep() {
+		bot.sleep(1000);
+	}
+}
